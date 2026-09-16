097dcc982bcda3209515bd45d876caecde2b1368
wadedeveloper56 <wade4615@gmail.com>
2026-09-16 08:16:08 -0500

    Extract ASTPrinter to separate files
    Add ASTPrinter.h/cpp implementing ASTVisitor methods (Program, VarDecl, FunctionDecl, ReturnStmt). Remove the inline ASTPrinter from JWcc2.cpp and include ASTPrinter.h. Update JWcc2.vcxproj and .filters to add the new source and header files. Keeps AST printing logic modular and reusable.

---
+++ source/Tools/JWcc2/ASTPrinter.cpp
@@ -0,0 +1,40 @@
+#include "ASTPrinter.h"
+
+void ASTPrinter::visit(ProgramNode* node)
+{
+	cout << "--- AST PROGRAM START ---\n";
+	for (auto& decl : node->externalDeclarations)
+	{
+		decl->accept(this);
+	}
+	cout << "--- AST PROGRAM END ---\n";
+}
+
+void ASTPrinter::visit(VarDeclNode* node)
+{
+	cout << (node->isGlobal ? "[Global Var] " : "[Local Var] ")	<< node->type << " " << node->name;
+	if (!node->initValue.empty())
+	{
+		cout << " = " << node->initValue;
+	}
+	cout << ";\n";
+}
+
+void ASTPrinter::visit(FunctionDeclNode* node)
+{
+	cout << "[Function] " << node->returnType << " " << node->name << "(";
+	for (size_t i = 0; i < node->params.size(); ++i)
+	{
+		cout << node->params[i].first << " " << node->params[i].second;
+		if (i + 1 < node->params.size()) cout << ", ";
+	}
+	cout << ") {\n";
+	for (auto& stmt : node->body)
+	{
+		cout << "  ";
+		stmt->accept(this);
+	}
+	cout << "}\n";
+}
+
+void ASTPrinter::visit(ReturnStmtNode* node) { cout << "return " << node->value << ";\n"; }
--- source/Tools/JWcc2/JWcc2.cpp
+++ source/Tools/JWcc2/JWcc2.cpp
@@ -2,51 +2,9 @@
 #include <vector>
 #include "Lexer.h"
 #include "Parser.h"
+#include "ASTPrinter.h"
 
 using namespace std;
-
-class ASTPrinter : public ASTVisitor
-{
-public:
-	void visit(ProgramNode* node) override 
-	{
-		cout << "--- AST PROGRAM START ---\n";
-		for (auto& decl : node->externalDeclarations)
-		{
-			decl->accept(this);
-		}
-		cout << "--- AST PROGRAM END ---\n";
-	}
-
-	void visit(VarDeclNode* node) override 
-	{
-		cout << (node->isGlobal ? "[Global Var] " : "[Local Var] ")
-			<< node->type << " " << node->name;
-		if (!node->initValue.empty())
-		{
-			cout << " = " << node->initValue;
-		}
-		cout << ";\n";
-	}
-
-	void visit(FunctionDeclNode* node) override {
-		cout << "[Function] " << node->returnType << " " << node->name << "(";
-		for (size_t i = 0; i < node->params.size(); ++i)
-		{
-			cout << node->params[i].first << " " << node->params[i].second;
-			if (i + 1 < node->params.size()) cout << ", ";
-		}
-		cout << ") {\n";
-		for (auto& stmt : node->body)
-		{
-			cout << "  ";
-			stmt->accept(this);
-		}
-		cout << "}\n";
-	}
-
-	void visit(ReturnStmtNode* node) override { cout << "return " << node->value << ";\n"; }
-};
 
 int main()
 {
