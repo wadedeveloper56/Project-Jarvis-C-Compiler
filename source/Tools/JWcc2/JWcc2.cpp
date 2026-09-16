c0f6461ed225b69a3509541feb2751f487678961
wadedeveloper56 <wade4615@gmail.com>
2026-09-16 09:57:32 -0500

    Add argument parsing to JWcc2; enable code analysis
    JWcc2: add argument parsing using argtable (bitsize, output, help, version, input files), default values, usage/version printing, and proper argtable cleanup; include ArgumentTable.h. JWcc2.vcxproj: add Arguments project reference and include path. Multiple Visual Studio project files (dosbox-x, freetype, libpdcurses, libpng, SDL, SDLmain, SDL_net, zlib, etc.) updated to enable RunCodeAnalysis and, where applicable, EnableMicrosoftCodeAnalysis for many build configurations. Binary PDB files updated.

--- source/Tools/JWcc2/JWcc2.cpp
+++ source/Tools/JWcc2/JWcc2.cpp
@@ -2,12 +2,93 @@
 #include "Lexer.h"
 #include "Parser.h"
 #include "ASTPrinter.h"
+#include "ArgumentTable.h"
 
 using namespace std;
-
-int main()
+bool bit16 = false;
+bool bit32 = false;
+bool bit64 = false;
+int main(int argc, char* argv[])
 {
 	string sourceCode = R"(int global_variable = 42;int standard_var;int main(int argc, int argv) {int local_var = 10;return local_var;})";
+
+	ArgIntPtr bitsize = argInt0("bB", "bitsize", NULL, "define bit size to be 16, 32 or 64 bits (default is 32)");
+	ArgFilePtr outfile = argFile0("Oo", "output", "<file>", "output file (default is \"-\")");
+	ArgLitPtr help = argLit0("hH", "help", "print this help and exit");
+	ArgLitPtr version = argLit0("vV", "version", "print version information and exit");
+	ArgFilePtr infiles = argFileN(NULL, NULL, NULL, 1, argc + 2, "input file(s)");
+	ArgEndPtr end = argEnd(20);
+	void* argtable[] = { bitsize, outfile, help, version, infiles, end };
+	const char* progname = "Compiler";
+	int exitcode = 0, nerrors = 0;
+
+	printf("Compiler (x86/x64) v1.0 (C) Copyright 2025-2026 Christopher D. Wade.\n");
+	printf("All Rights Reserved\n");
+
+	if (argNullCheck(argtable) != 0)
+	{
+		printf("%s: insufficient memory\n", progname);
+		exitcode = 1;
+		goto exit;
+	}
+	bitsize->ival[0] = 32;
+	outfile->filename[0] = "-";
+
+	nerrors = argParse(argc, argv, argtable);
+
+	if (bitsize->ival[0] == 16)
+		bit16 = true;
+	else if (bitsize->ival[0] == 32)
+		bit32 = true;
+	else
+		bit64 = true;
+
+	if (help->count > 0)
+	{
+		printf("Usage: %s", progname);
+		argPrintSyntax(stdout, argtable, "\n");
+		printf("Compiler that coverts c code to 16,32 or 64 bit assembler\n");
+		argPrintGlossary(stdout, argtable, "  %-25s %s\n");
+		exitcode = 0;
+		goto exit;
+	}
+
+	if (version->count > 0)
+	{
+		printf("'%s' c code to assembler compiler.\n", progname);
+		printf("June 2025-2026, (c) Christopher D. Wade\n");
+		exitcode = 0;
+		goto exit;
+	}
+
+	if (nerrors > 0)
+	{
+		argPrintErrors(stdout, end, progname);
+		printf("Try '%s --help' for more information.\n", progname);
+		exitcode = 1;
+		goto exit;
+	}
+
+	if (argc == 1)
+	{
+		printf("Try '%s --help' for more information.\n", progname);
+		exitcode = 0;
+		goto exit;
+	}
+
+	if (infiles->count == 0)
+	{
+		printf("No input file specified.\n");
+		exitcode = 1;
+		goto exit;
+	}
+
+	if (outfile->count == 0)
+	{
+		printf("No output file specified.\n");
+		exitcode = 1;
+		goto exit;
+	}
 	try
 	{
 		Lexer lexer(sourceCode);
@@ -22,5 +103,8 @@ int main()
 		cerr << e.what() << endl;
 		return 1;
 	}
-	return 0;
+
+exit:
+	argFreeTable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+	return exitcode;
 }
