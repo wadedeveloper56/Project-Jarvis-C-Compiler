5fdbd0c7eb84ebf97d0f8863fb3c3a9e0dcc317e
wadedeveloper56 <wade4615@gmail.com>
2026-09-16 20:33:08 -0500

    Add native unit test projects
    Added Microsoft CppUnitTest-based projects for FileTest and JWcc2Test, converted the project files to the VS native unit test template, and updated the solution to include them. Replaced the previous gtest scaffolding with precompiled-header setup and removed stale package/config files. Refreshed generated PDB binaries as part of the build output updates.

---
+++ source/Tools/FileTest/FileTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FileTest
+{
+	TEST_CLASS(FileTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ source/Tools/JWcc2Test/JWcc2Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace JWcc2Test
+{
+	TEST_CLASS(JWcc2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
