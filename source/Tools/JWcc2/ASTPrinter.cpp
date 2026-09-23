f9d0df047f6385339e758860efb30985dcd32390
wadedeveloper56 <wade4615@gmail.com>
2026-09-23 09:19:13 -0500

    Refactor parser and AST printer implementation
    This change moves the ASTPrinter and Parser member implementations out of the headers and into their corresponding .cpp files. It keeps the public APIs declared in the headers while preserving the parsing logic for declarations, statements, expressions, and type handling, making the code cleaner and reducing header coupling.

--- source/Tools/JWcc2/ASTPrinter.cpp
+++ source/Tools/JWcc2/ASTPrinter.cpp
@@ -1,3 +1,77 @@
 #include "pch.h"
 #include "ASTPrinter.h"
 
+using namespace std;
+
+void ASTPrinter::ind() { for (int i = 0; i < indent; ++i) cout << "  "; }
+//void printLoc(const Node& n) { cout << " [" << n.loc.startLine << ":" << n.loc.startColumn << "-" << n.loc.endLine << ":" << n.loc.endColumn << "]"; }
+
+void ASTPrinter::visit(Program& n) {
+	cout << "Program\n";
+	indent++;
+	for (auto& d : n.declarations) { d->accept(*this); }
+	indent--;
+}
+
+void ASTPrinter::visit(VariableDeclaration& n) {
+	ind(); cout << "VariableDeclaration type=" << n.type << " name=" << n.name;
+	// printLoc(n);
+	if (n.init) { cout << " init=\n"; indent++; n.init->accept(*this); indent--; }
+	else cout << "\n";
+}
+
+void ASTPrinter::visit(FunctionDeclaration& n) {
+	ind(); cout << "FunctionDeclaration ret=" << n.retType << " name=" << n.name << "\n";
+	// printLoc(n);
+	indent++;
+	ind(); cout << "Params:\n"; indent++;
+	for (auto& p : n.params) { ind(); cout << p.first << " " << p.second << "\n"; }
+	indent--;
+	ind(); cout << "Body:\n"; indent++;
+	if (n.body) n.body->accept(*this);
+	indent -= 2;
+}
+
+void ASTPrinter::visit(CompoundStatement& n) {
+	ind(); cout << "CompoundStatement\n";
+	// printLoc(n);
+	indent++;
+	for (auto& d : n.localDeclarations) d->accept(*this);
+	for (auto& s : n.stmts) s->accept(*this);
+	indent--;
+}
+
+void ASTPrinter::visit(ReturnStatement& n) {
+	ind(); cout << "Return\n";
+	if (n.expr) { indent++; n.expr->accept(*this); indent--; }
+}
+
+void ASTPrinter::visit(ExpressionStatement& n) {
+	ind(); cout << "ExpressionStatement\n";
+	if (n.expr) { indent++; n.expr->accept(*this); indent--; }
+}
+
+void ASTPrinter::visit(NumberExpression& n) {
+	ind(); cout << "Number " << n.value << "\n";
+}
+
+void ASTPrinter::visit(VariableExpression& n) {
+	ind(); cout << "Variable " << n.name << "\n";
+}
+
+void ASTPrinter::visit(BinaryExpression& n) {
+	ind(); cout << "Binary op=" << n.op << "\n";
+	indent++; n.lhs->accept(*this); n.rhs->accept(*this); indent--;
+}
+
+void ASTPrinter::visit(AssignExpression& n) {
+	ind(); cout << "Assign to " << n.name << "\n";
+	indent++; n.value->accept(*this); indent--;
+}
+
+void ASTPrinter::visit(CallExpression& n) {
+	ind(); cout << "Call " << n.callee << "\n";
+	indent++;
+	for (auto& a : n.args) a->accept(*this);
+	indent--;
+}
--- source/Tools/JWcc2/ASTPrinter.h
+++ source/Tools/JWcc2/ASTPrinter.h
@@ -6,66 +6,17 @@
 struct ASTPrinter : ASTVisitor
 {
     int indent = 0;
-    void ind() { for (int i = 0; i < indent; ++i) cout << "  "; }
-    //void printLoc(const Node& n) { cout << " [" << n.loc.startLine << ":" << n.loc.startColumn << "-" << n.loc.endLine << ":" << n.loc.endColumn << "]"; }
-
-    void visit(Program& n) override {
-        cout << "Program\n";
-        indent++;
-        for (auto& d : n.declarations) { d->accept(*this); }
-        indent--;
-    }
-    void visit(VariableDeclaration& n) override {
-        ind(); cout << "VariableDeclaration type=" << n.type << " name=" << n.name;
-        // printLoc(n);
-        if (n.init) { cout << " init=\n"; indent++; n.init->accept(*this); indent--; }
-        else cout << "\n";
-    }
-    void visit(FunctionDeclaration& n) override {
-        ind(); cout << "FunctionDeclaration ret=" << n.retType << " name=" << n.name << "\n";
-        // printLoc(n);
-        indent++;
-        ind(); cout << "Params:\n"; indent++;
-        for (auto& p : n.params) { ind(); cout << p.first << " " << p.second << "\n"; }
-        indent--;
-        ind(); cout << "Body:\n"; indent++;
-        if (n.body) n.body->accept(*this);
-        indent -= 2;
-    }
-    void visit(CompoundStatement& n) override {
-        ind(); cout << "CompoundStatement\n";
-        // printLoc(n);
-        indent++;
-        for (auto& d : n.localDeclarations) d->accept(*this);
-        for (auto& s : n.stmts) s->accept(*this);
-        indent--;
-    }
-    void visit(ReturnStatement& n) override {
-        ind(); cout << "Return\n";
-        if (n.expr) { indent++; n.expr->accept(*this); indent--; }
-    }
-    void visit(ExpressionStatement& n) override {
-        ind(); cout << "ExpressionStatement\n";
-        if (n.expr) { indent++; n.expr->accept(*this); indent--; }
-    }
-    void visit(NumberExpression& n) override {
-        ind(); cout << "Number " << n.value << "\n";
-    }
-    void visit(VariableExpression& n) override {
-        ind(); cout << "Variable " << n.name << "\n";
-    }
-    void visit(BinaryExpression& n) override {
-        ind(); cout << "Binary op=" << n.op << "\n";
-        indent++; n.lhs->accept(*this); n.rhs->accept(*this); indent--;
-    }
-    void visit(AssignExpression& n) override {
-        ind(); cout << "Assign to " << n.name << "\n";
-        indent++; n.value->accept(*this); indent--;
-    }
-    void visit(CallExpression& n) override {
-        ind(); cout << "Call " << n.callee << "\n";
-        indent++;
-        for (auto& a : n.args) a->accept(*this);
-        indent--;
-    }
+    void ind();
+    //void printLoc(const Node& n);
+    void visit(Program& n) override;
+    void visit(VariableDeclaration& n) override;
+    void visit(FunctionDeclaration& n) override;
+    void visit(CompoundStatement& n) override;
+    void visit(ReturnStatement& n) override;
+    void visit(ExpressionStatement& n) override;
+    void visit(NumberExpression& n) override;
+    void visit(VariableExpression& n) override;
+    void visit(BinaryExpression& n) override;
+    void visit(AssignExpression& n) override;
+    void visit(CallExpression& n) override;
 };
